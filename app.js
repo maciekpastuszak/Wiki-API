c016050d4b00888f697a8095a1538f98e6c01812
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2022-12-12 09:06:11 +0100

    add route to fetch all articles

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 const express = require('express');
-const app = express();
 const ejs = require('ejs');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+
+const app = express();
+
+mongoose.connect("mongodb://localhost:27017/wikiDB");
 
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({
     extended : true
 }));
-
 app.use(express.static('public'));
-
-mongoose.connect("mongodb://localhost:27017/wikiDB");
 
 const articleSchema = {
     title: String,
@@ -21,6 +21,16 @@ const articleSchema = {
 
 const Article = mongoose.model("Article", articleSchema);
 
+app.get("/articles", (req,res) => {
+    Article.find((err, foundArticles) => {
+        if(!err){
+            res.send(foundArticles);
+        } else {
+            res.send(err)
+        }
+    })
+});
+
 app.listen(3000, () => {
     console.log("Server started on port 3000");
-})
+});
